b293c620a16c2a56e92b91c338bb9efb6158c9e8
obcido <jonathan.harkonen@gmail.com>
2021-09-02 16:07:32 +0300

    add dropshadow hover-effect

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-window.onload = function() {
+window.onload = function () {
     // Lyssna efter händelser
+    let elements = document.getElementsByClassName('team-member');
+
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].addEventListener('mouseover', effectOn, false);
+        elements[i].addEventListener('mouseout', effectOff, false);
+    }
 }
-var editTitle = function(e) {
+var editTitle = function (e) {
     // Ändra "team-title"
 };
 
-function effectOn(id){
+function effectOn(id) {
     // Rita en ram runt personen, kanske lite drop shadow?
+    id.currentTarget.style.setProperty('-webkit-filter', 'drop-shadow(2px 4px 4px #333)');
+
 }
 
-function effectOff(id){
+function effectOff(id) {
     // Stäng av effekten när musen inte längre är ovanför personen
+    id.currentTarget.style.removeProperty('-webkit-filter', 'drop-shadow(2px 3px 3px #333)');
 }
