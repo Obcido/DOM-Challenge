2685f65a58c85f97c6b3a2a4c69107acc432f943
obcido <jonathan.harkonen@gmail.com>
2021-09-02 16:23:03 +0300

    add prompt att byta arbetstitel

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 window.onload = function () {
     // Lyssna efter händelser
     let elements = document.getElementsByClassName('team-member');
+    let titles = document.getElementsByClassName('team-title');
 
     for (let i = 0; i < elements.length; i++) {
         elements[i].addEventListener('mouseover', effectOn, false);
         elements[i].addEventListener('mouseout', effectOff, false);
+        titles[i].addEventListener('click', editTitle, false);
     }
+
+
+
 }
 var editTitle = function (e) {
     // Ändra "team-title"
+    let newTitle = prompt('Ny titel: ', e.currentTarget.innerText);
+    e.currentTarget.innerText = newTitle;
 };
 
 function effectOn(id) {
